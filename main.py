2f7ebdfb79ae2b00d78f542b612e68082adefc55
Ngoc <ngockhanhmai1102@gmail.com>
2024-12-22 21:00:34 +0000

    define greeting function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print("Welcome to the Chatbot")
-name = input("What is your name")
-
 def greeting():
+    print("Welcome to the Chatbot")
     name = input("What is your name? ")
+    age = input("Hello " + name + ", how old are you? ")
+
+greeting()
